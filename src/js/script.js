3458882ca897b7b7d9b332150e029c801d651c6c
Eugene <daria.choganina@gmail.com>
2022-07-20 19:49:17 +0300

    6

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,38 @@
-// $(document).ready(function () {
-// 	$('.carousel__inner').slick({
-// 		speed: 1200,
-// 		adaptiveHeight: true,
-// 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-// 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-// 		responsive: [
-// 			{
-// 				breakpoint: 992,
-// 				settings: {
-// 					dots: true,
-// 					arrows: false
-// 				}
-// 			}
-// 		]
-// 	});
-// });
-
-const slider = tns({
-	container: '.carousel__inner',
-	items: 1,
-	slideBy: 'page',
-	autoplay: false,
-	controls: false,
-	nav: false,
-	autoHeight: true,
-	responsive: {
-		640: {
-			edgePadding: 20,
-			gutter: 20,
-			items: 1
-		},
-	}
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 1200,
+		adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+	});
 });
-document.querySelector('.prev').onclick = function () {
-	slider.goTo('prev');
-};
-document.querySelector('.next').onclick = function () {
-	slider.goTo('next');
-};
+
+// const slider = tns({
+// 	container: '.carousel__inner',
+// 	items: 1,
+// 	slideBy: 'page',
+// 	autoplay: false,
+// 	controls: false,
+// 	nav: false,
+// 	autoHeight: true,
+// 	responsive: {
+// 		900: {
+// 			nav: true;
+// 		}
+// 	}
+// });
+// document.querySelector('.prev').onclick = function () {
+// 	slider.goTo('prev');
+// };
+// document.querySelector('.next').onclick = function () {
+// 	slider.goTo('next');
+// };
